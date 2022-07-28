687a8b90cf2d541a71bcbbc63b8320b5d179a462
siro988 <siro988@gmail.com>
2022-07-28 13:34:54 -0400

    Change colers and finished carrousel

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,27 +3,29 @@ import React, { useState, useEffect } from 'react'
 
 import { getCategories } from '../services'
 
-function Header() {
+const Header = () => {
     const [categories, setcategories] = useState([])
 
     useEffect(() => {
         getCategories()
-            .then((newCategories) => setcategories(newCategories))
+            .then((newCategories) => {
+                setcategories(newCategories);
+            })
     }, [])
     return (
         <div className='container mx-auto px-10 mb-8'>
-            <div className='border-b w-full inline-block border-green-600 py-8'>
+            <div className='border-b w-full inline-block border-black py-8'>
                 <div className='md:float-left block'>
                     <Link href='/'>
-                        <span className='cursor-pointer font-bold text-4xl text-white'>
-                            GraphCMS
+                        <span className='cursor-pointer font-bold text-4xl text-gray-700'>
+                            Food Blog
                         </span>
                     </Link>
                 </div>
                 <div className='hidden md:float-left md:contents'>
-                    {categories.map((category) => (
-                        <Link key={category.slug} href={`/categories/${category.slug}`}>
-                            <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
+                    {categories.map((category, index) => (
+                        <Link key={category.slug} href={`/category/${category.slug}`}>
+                            <span className='md:float-right mt-2 align-middle text-grey-700 ml-4 font-semibold cursor-pointer'>
                                 {category.name}
                             </span>
                         </Link>
