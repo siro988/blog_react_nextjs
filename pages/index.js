088463ef409712d826fa22ca67bd85049d667225
siro988 <siro988@gmail.com>
2022-08-10 08:33:37 -0400

    Removed comment

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import { FeaturedPosts } from '../sections/index';
 import { PostCard, Categories, PostWidgets } from '../components';
 import { getPosts } from '../services';
-// test
 
 export default function Home({ posts }) {
   return (
